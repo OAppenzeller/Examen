71f51198b3a21f479dab10b511c72dd017c62598
OAppenzeller <o.appenzeller@yahoo.com>
2024-11-06 18:54:20 -0600

    Updated the Program cs

--- Examen/Program.cs
+++ Examen/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Examen;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Asignatura asignatura = new Asignatura();
+
+        try
+        {
+            Console.Write("Ingrese el nombre del alumno: ");
+            asignatura.NombreAlumno = Console.ReadLine();
+
+            Console.Write("Ingrese el numero de cuenta: ");
+            asignatura.NumeroCuenta = Console.ReadLine();
+
+            Console.Write("Ingrese el email: ");
+            asignatura.Email = Console.ReadLine();
+
+            Console.Write("Ingrese el nombre de la asignatura: ");
+            asignatura.NombreAsignatura = Console.ReadLine();
+
+            Console.Write("Ingrese el nombre del docente: ");
+            asignatura.NombreDocente = Console.ReadLine();
+
+            Console.Write("Ingrese el horario: ");
+            asignatura.Horario = Console.ReadLine();
+
+
+        }catch(FormatException)
+        {
+            Console.WriteLine("Error de formato");
+        }
+
+    }
+}
